e7eeb81957a85b98b67b0b583f1693b7662a4481
Jaibw <jai.unix@gmail.com>
2023-04-18 15:38:38 +0530

    Update AddTest.java

--- src/test/java/com/example/project/AddTest.java
+++ src/test/java/com/example/project/AddTest.java
@@ -13,7 +13,7 @@ class AddTest {
 	@Test
 	void myAddTest(TestInfo testInfo) {
 		Calculator calculator = new Calculator();
-		assertEquals(11, calculator.add(1, 1), "1 + 1 should equal 11");
+		assertEquals(3, calculator.add(1, 2), "1 + 2 should equal 3");
 		
 	}
 
